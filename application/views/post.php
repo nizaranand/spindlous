28ba70b2f9dd0eeae0c1e31bc233dd6bd8a524be
John Sparwasser <johnspar1@gmail.com>
2011-10-19 17:56:18 -0400

    10/19/11

--- application/views/post.php
+++ application/views/post.php
@@ -1,30 +1,32 @@
 <div id="post_container">
-
-
 	<div class="outer-post-container">
 		<div class="picture">
 			<img src="<?php echo base_url() . $post['profile_pic']; ?>" height="35" width="35"/>
 			<p class="author"><a href="<?php echo base_url() . $post['author'];?>" ><?php echo $post['author']; ?></a></p>
 		</div> 				
 		
-		<div id="<?php echo $post['sid']; ?>"class="post-container-1">
+
 		
-			<?php if ($post['title'] != '') { ?>
+		<?php if ($post['title'] != '') { ?>
+			<div id="<?php echo $post['sid']; ?>"class="post-container">
 				<h2><a href="<?php echo $post['url']; ?>" class="post-title"><?php echo $post['title']; ?></a></h2>
 				<div class="post-body-container">
 					<p class="body"><?php echo $post['body']; ?></p>
 					<p class="comments"><a href="<?php echo $post['url']; ?>" >Comments[<?php echo $post['ncomments'];?>] Shares[<?php echo $post['nshares'];?>]</a></p>
 				</div>
-			<?php } else { ?>
-				<div class="post-body-container">
+			</div>
+		<?php } else { ?>
+			<div id="<?php echo $post['sid']; ?>"class="little-post-container">
+				<div class="-post-body-container">
 					<p class="little-body"><?php echo $post['body']; ?></p>
 					<p class="little-comments"><a href="<?php echo $post['url']; ?>" >Comments[<?php echo $post['ncomments'];?>] Shares[<?php echo $post['nshares'];?>]</a></p>
 				</div>
-			<?php } ?>
+			</div>
+		<?php } ?>
 			
 			
 			
-		</div>
+		
 	</div>
 	
 	<div id="tab-container">
